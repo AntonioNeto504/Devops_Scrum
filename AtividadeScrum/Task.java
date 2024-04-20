7bfe8f60172b67042997639bff131f77e4413f8b
AntonioNeto504 <antoniocustorioneto@gmail.com>
2024-04-19 22:50:57 -0300

    JavaScrum

---
+++ AtividadeScrum/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    private String description;
+    private boolean completed;
+  
+    public Task(String description, boolean completed){
+     this.completed = completed;
+     this.description = description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setCompleted(boolean completed) {
+     this.completed = completed;
+ }
+    public String getDescription() {
+        return description;
+    }    
+    public boolean isCompleted() {
+        return completed;
+    }
+  
+    public void mark_as_completed(){
+     this.completed = true;
+    }
+ }
+  
